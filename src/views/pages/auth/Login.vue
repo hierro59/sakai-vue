4d085431f4b52c8b75efeecbd58210cc5581b521
felix.leon <felix.leon@griky.co>
2025-06-13 11:12:15 -0500

    Fixed login view

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -36,12 +36,12 @@ const login = async () => {
 </script>
 
 <template>
-    <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
+    <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen overflow-hidden">
         <div class="flex flex-col items-center justify-center">
             <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                 <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                     <div class="text-center mb-8">
-                        <img :src="companyLogo" :alt="company" class="mb-4" />
+                        <img :src="companyLogo" :alt="company.name" class="mb-12 mx-auto max-w-[250px]" />
                         <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Welcome</div>
                         <span class="text-muted-color font-medium">Log in to continue</span>
                     </div>
